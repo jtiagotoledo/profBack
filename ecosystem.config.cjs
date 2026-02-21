a5ccde019d533448709557cc690a5248217ce7bb
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-02-21 10:41:58 -0300

    criado arquivo para pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name            : "profBack",
+    script          : "server.js",
+    node_args       : "--experimental-modules", 
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },
+    log_date_format : "YYYY-MM-DD HH:mm:ss",
+    error_file      : "./logs/err.log",
+    out_file        : "./logs/out.log",
+    merge_logs      : true
+  }]
+};
