e8ea2610178eeb460e35237754d70ab50f83c1e1
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-02-21 11:23:36 -0300

    rota de inserção de dados teste v3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); 
 
-import Atividade from './models/Atividade.js';
+import Atividade from './models/atividades.js';
 import connectDB from './config/db.js';
 
 const app = express();
