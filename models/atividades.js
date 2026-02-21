1ff16daa763fcbd7767e989b3d73ee20212dea2b
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-02-21 11:17:45 -0300

    rota de inserção de dados teste v2

--- models/atividades.js
+++ models/atividades.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const AtividadeSchema = new mongoose.Schema({
   titulo: { type: String, required: true }, 
+  descricao: { type: String },
   materia: { type: String, default: 'Robótica' },
   dataCriacao: { type: Date, default: Date.now }
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); 
 
+import Atividade from './models/Atividade.js';
 import connectDB from './config/db.js';
 
 const app = express();
