3aad0e9fb2683c0822cf4d791388dca959fd9dd6
jtiagotoledo <jtiagotoledo@gmail.com>
2026-03-28 06:26:46 -0300

    adicionado limites de criação de classes no backend v2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { protect } from '../middlewares/authMiddleware.js';
 import { googleLogin } from '../controllers/authController.js';
 
 const router = express.Router();
