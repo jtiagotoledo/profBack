5fac5852a8a5531b170335be4b7366778ccdf8e9
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-07-23 14:42:00 -0300

    horario do professor v7

--- controllers/horarioController.js
+++ controllers/horarioController.js
@@ -2,7 +2,7 @@ import Horario from '../models/HorarioModel.js';
 
 export const getHorarios = async (req, res) => {
     try {
-        const horarios = await Horario.find({ usuarioId: req.usuarioId })
+        const horarios = await Horario.find({ usuarioId: req.user._id })
                                       .populate('classeId'); 
         res.json(horarios);
     } catch (error) {
@@ -12,12 +12,18 @@ export const getHorarios = async (req, res) => {
 
 export const saveHorario = async (req, res) => {
     const { diaSemana, aula, horario, classeId } = req.body;
+    
+    const professorId = req.user._id; 
 
     try {
         const horarioSalvo = await Horario.findOneAndUpdate(
-            { usuarioId: req.usuarioId, diaSemana, aula }, 
-            { horario, classeId },                         
-            { new: true, upsert: true }                    
+            { usuarioId: professorId, diaSemana, aula }, 
+            { 
+                horario, 
+                classeId,
+                usuarioId: professorId 
+            },                         
+            { returnDocument: 'after', upsert: true }                    
         ).populate('classeId');
 
         res.json(horarioSalvo);
@@ -30,7 +36,7 @@ export const deleteHorario = async (req, res) => {
     const { id } = req.params;
     
     try {
-        await Horario.findOneAndDelete({ _id: id, usuarioId: req.usuarioId });
+        await Horario.findOneAndDelete({ _id: id, usuarioId: req.user._id });
         res.json({ message: 'Horário removido com sucesso.' });
     } catch (error) {
         res.status(500).json({ error: 'Erro ao remover o horário.' });
